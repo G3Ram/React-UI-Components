84cec03b7eabf77f3cd0397301b19d8d5651538e
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-04-22 12:53:10 -0500

    Social-Card Project: Added header content
    Added the header title and header text and aligned it with the design provided using pexels.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Header.css";
 import ImageThumbnail from "./ImageThumbnail";
+import HeaderContent from "./HeaderContent";
 
 const HeaderContainer = () => {
   return (
     <div className="header">
       <ImageThumbnail />
+      <HeaderContent />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="headerTitle">
+      <h1>Lambda School</h1>
+      <span className="subTitle"> testing </span>
+    </div>
+  );
+};
+
+export default HeaderTitle;
