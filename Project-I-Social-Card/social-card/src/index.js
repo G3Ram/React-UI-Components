bd60f4149ea431b22cbf86711fbcfdfffdc2c2ce
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-04-22 15:27:51 -0500

    Social-Card Project: Stretch - Added moment.js
    The current date is displayed using the moment.js

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -2,17 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
-import CardContainer from "./components/CardComponents/CardContainer";
+// import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
+// import CardContainer from "./components/CardComponents/CardContainer";
 
-const Index = () => {
-  return (
-    <div>
-      <HeaderContainer />
-      <CardContainer />
-    </div>
-  );
-};
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+//ReactDOM.render(<Index />, document.getElementById("root"));
