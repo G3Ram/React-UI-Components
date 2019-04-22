ccc14b01239a985cf26437c041f1a6a9e4843e98
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-04-22 10:57:06 -0500

    Social-Card Project: Added Lambda Logo
    Updated the Header components. Created a HeaderContainer and ImageThumbnail component. Added the Thumbnail component to HeaderContainer and rendered the container in index.js

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HeaderContainer />, document.getElementById("root"));
